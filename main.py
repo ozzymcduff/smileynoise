dd7dd4f4b3443274f4c36b904ab97c0d81d748ab
Oskar Gewalli <gewalli@gmail.com>
2010-04-28 00:17:55 +0200

    some bug fixes related to the problem with non ascii chars

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-
-
 from google.appengine.ext import db
 from google.appengine.api import users
 from model import *
@@ -34,14 +16,13 @@ class MainHandler(webapp.RequestHandler):
 		txt = []
 		for item in data:
 			txt.append({'id':item.key().id(),'value':item.value,\
-				'writer':item.writer.nickname(),\
-				'urlid': urllib.quote_plus(item.value) } )
+				'writer':item.writer.nickname() } )
 		self.response.out.write(template.render('views/main/index.html',{'messages':txt}))
 
 class ViewMessage(webapp.RequestHandler):
-	def get(self,value):
-		item = Message.gql("WHERE value = :value",value=urllib.unquote_plus(value)).get()
-		self.response.out.write(template.render('views/main/view.html',{ 'data':item, 'url': urllib.quote_plus("http://smileynoise.appspot.com/view/"+item.value)}))
+	def get(self,id):
+		item = Message.get_by_id(ids=int(id),parent=None)
+		self.response.out.write(template.render('views/main/view.html',{ 'data':item, 'url': urllib.quote_plus("http://smileynoise.appspot.com/view/"+str(item.key().id()))}))
 
 
 def main():
